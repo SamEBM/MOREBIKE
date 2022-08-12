0ae9a75d8f8981b9005bbff0978e899c26384ddf
Samuel E. Benitez Miranda <samuelebm@Samuels-MacBook-Air.local>
2022-08-11 22:15:35 -0500

    Adding destination neighborhoods page content

--- morebike/src/app/constants/visualizations.ts
+++ morebike/src/app/constants/visualizations.ts
@@ -26,9 +26,24 @@ export const MOREBIKE = {
     destination: {
         graphs: [
             {
-                title: "",
-                description: "",
-                tableauHTML: ""
+                title: "Pareto Charts",
+                description: 'Estas visualizaciones buscan identificar si se cumple el principio de Pareto (también conocido como la regla del 80/20) en los viajes mensuales por colonia en los distintos años del sistema. Observamos cuáles son las colonias que comúnmente aportan una mayor proporción de viajes entre semana y en fin de semana, las cuales conforman el 80% de los viajes totales por mes.',
+                tableauURL: "https://public.tableau.com/views/ParetoDestino/ParetoDestino?:language=en-US&:display_count=n&:origin=viz_share_link"
+            },
+            {
+                title: "Box Plots",
+                description: 'Estas visualizaciones buscan identificar si se cumple el principio de Pareto (también conocido como la regla del 80/20) en los viajes mensuales por colonia en los distintos años del sistema. Observamos cuáles son las colonias que comúnmente aportan una mayor proporción de viajes entre semana y en fin de semana, las cuales conforman el 80% de los viajes totales por mes.',
+                tableauURL: "https://public.tableau.com/views/BoxplotsDestino/CuartilesDestino?:language=en-US&:display_count=n&:origin=viz_share_link"
+            },
+            {
+                title: "Neighborhood-to-Neighborhood Mobility",
+                description: 'Estas visualizaciones buscan identificar si se cumple el principio de Pareto (también conocido como la regla del 80/20) en los viajes mensuales por colonia en los distintos años del sistema. Observamos cuáles son las colonias que comúnmente aportan una mayor proporción de viajes entre semana y en fin de semana, las cuales conforman el 80% de los viajes totales por mes.',
+                tableauURL: "https://public.tableau.com/views/ColoniasDestino_Orgenes/ColoniasDestino-Orgenes?:language=en-US&:display_count=n&:origin=viz_share_link"
+            },
+            {
+                title: "Weekly Regular Mobility",
+                description: 'Estas visualizaciones buscan identificar si se cumple el principio de Pareto (también conocido como la regla del 80/20) en los viajes mensuales por colonia en los distintos años del sistema. Observamos cuáles son las colonias que comúnmente aportan una mayor proporción de viajes entre semana y en fin de semana, las cuales conforman el 80% de los viajes totales por mes.',
+                tableauURL: "https://public.tableau.com/views/Patrones_ColoniaDestino/ResumenPatronesxColonia-Destino?:language=en-US&:display_count=n&:origin=viz_share_link"
             }
         ]
     }
--- morebike/src/app/tab3/tab3.page.ts
+++ morebike/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MOREBIKE } from '../constants/visualizations';
+declare var tableau: any;
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  counter: number = 0;
+  showVisualizations: Array<boolean> = new Array<boolean>();
+  visualizations: Array<any> = new Array<any>();
+  @ViewChild("vizContainer1") containerDiv1: ElementRef;
+  @ViewChild("vizContainer2") containerDiv2: ElementRef;
+  @ViewChild("vizContainer3") containerDiv3: ElementRef;
+  @ViewChild("vizContainer4") containerDiv4: ElementRef;
+  divContainers: Array<ElementRef> = new Array<ElementRef>();
 
-  constructor() {}
+  viz: Array<any> = new Array<any>;
 
+  tableauOptions = {
+    // width: '100%',
+    // height: '1000px',
+    device: 'desktop',
+    hideTabs: true,
+    onFirstInteractive: () => {
+      console.log("onFirstInteractive");
+    },
+    onFirstVizSizeKnown: () => {
+      console.log("onFirstVizSizeKnown");
+      this.showVisualizations[this.counter] = true;
+      this.counter++;
+    }
+  };
+
+  constructor(private elem: ElementRef) {
+
+  }
+
+  ngOnInit(): void {
+    this.visualizations = MOREBIKE.destination.graphs;
+  }
+
+  ngAfterViewInit() {
+    this.divContainers = this.elem.nativeElement.querySelectorAll('.dashboard');
+    console.log(this.divContainers)
+    this.initTableau();
+  }
+
+  initTableau() {
+    let vizUrl = '';
+    this.divContainers.forEach((visualization, index) => {
+      vizUrl = this.visualizations[index].tableauURL;
+      this.viz[index] = new tableau.Viz(
+        visualization,
+        vizUrl,
+        this.tableauOptions
+      );
+    });
+  }
 }
+
